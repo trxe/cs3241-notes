e2e7880a556db47bb33c52541ed29d99b10f81d8
trxe <peixianwong@gmail.com>
2022-08-29 08:11:48 +0800

    tut02 ready for tutorial

---
+++ tut02/template.cpp
@@ -0,0 +1,12 @@
+glViewport(u, v, w, h);
+glMatrixMode(           );
+glLoadIdentity(); /// Reset matrix
+
+double apparentHeight =             ;
+
+// Setup projection matrix
+if (                    ) {
+    gluOrtho2D();
+} else {
+    gluOrtho2D();
+}
